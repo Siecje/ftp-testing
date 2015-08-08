4bd82b2f63a65d95aadc7ff61eed3dc66b0ddf99
Cody Scott <cody.j.b.scott@gmail.com>
2015-08-08 14:26:22 -0400

    Threading working

--- test_ftp_fixture_one.py
+++ test_ftp_fixture_one.py
@@ -1,6 +1,12 @@
-from fixtures import run_ftp
-
-
 def test_one(run_ftp):
+    run_ftp.wait()
+    pass
+
+
+def test_four(run_ftp):
+    run_ftp.wait()
+    pass
+
+def test_five(run_ftp):
     run_ftp.wait()
     pass
--- test_ftp_fixture_two.py
+++ test_ftp_fixture_two.py
@@ -1,6 +1,7 @@
-from fixtures import run_ftp
-
-
 def test_two(run_ftp):
+    run_ftp.wait()
+    pass
+
+def test_three(run_ftp):
     run_ftp.wait()
     pass
